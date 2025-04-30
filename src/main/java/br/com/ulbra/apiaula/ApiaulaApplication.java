bd60494e18add7f0b19a7aff907ea23c75b77ffa
tamiresdasilva <tamiresmalvina@rede.ulbra.br>
2025-04-30 14:32:56 -0300

    first commit

---
+++ src/main/java/br/com/ulbra/apiaula/ApiaulaApplication.java
@@ -0,0 +1,13 @@
+package br.com.ulbra.apiaula;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiaulaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiaulaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/ulbra/apiaula/ApiaulaApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.ulbra.apiaula;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiaulaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
